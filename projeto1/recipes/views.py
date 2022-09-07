70bc9fb97f3da6e612cc9632f4160fc8d6c4cf05
KauaVicto <kauavictor259@gmail.com>
2022-09-06 21:17:28 -0300

    removido contato e sobre

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, "recipes/home.html", context={
         'name': 'Layla'
     })
-
-
-def sobre(request):
-    return render(request, "recipes/sobre.html")
-
-
-def contato(request):
-    return HttpResponse('CONTATO 1')
