40cd737b9213607de7447d62cb029471da3e72c7
KauaVicto <kauavictor259@gmail.com>
2022-10-23 08:37:44 -0300

    corrigido pagina de receita detalhada

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -1,6 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_object_or_404, render
 
 from recipes.models import Category, Recipe
 
@@ -15,26 +14,30 @@ def home(request):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id
+    ).first()
+
+    if recipe is None:
+        raise Http404('Receita não encontrada')
+        return
+
     return render(request, "recipes/pages/recipe-view.html", context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
 
 
 def category(request, category_id):
+    category_query = Category.objects.filter(id=category_id)
+    category = get_object_or_404(category_query)
+
     recipes = Recipe.objects.filter(
         category__id=category_id,  # Todas as receitas de uma categoria
         is_published=True  # Apenas as receitas que foram publicadas
     ).order_by('-id')
 
-    category = Category.objects.filter(
-        id=category_id
-    ).first()
-
-    if category is None:
-        raise Http404('Not found 😊')
-    else:
-        return render(request, "recipes/pages/category.html", context={
-            'recipes': recipes,
-            'title': f'{category.name} | Category'
-        })
+    return render(request, "recipes/pages/category.html", context={
+        'recipes': recipes,
+        'title': f'{category.name} | Category'
+    })
