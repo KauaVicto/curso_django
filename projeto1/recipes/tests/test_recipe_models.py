ddebb6b72949c6d29ba7f044b1f01ad3fd8dfb4e
KauaVicto <kauavictor259@gmail.com>
2022-12-14 22:57:15 -0300

    coveragerc

--- projeto1/recipes/tests/test_recipe_models.py
+++ projeto1/recipes/tests/test_recipe_models.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
 class RecipeModelsTest(RecipeTestBase):
@@ -8,22 +9,42 @@ class RecipeModelsTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = '-' * 66
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+
+        setattr(self.recipe, field, 'A' * (max_length + 1))
 
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
 
-    def test_recipe_fields_max_length(self):
-        fields = [
-            ('title', 65),
-            ('description', 165),
-            ('preparation_time_unit', 65),
-            ('servings_unit', 65),
-        ]
+    @parameterized.expand([
+        ('is_published', False),
+        ('preparation_steps_is_html', False)
+    ])
+    def test_recipe_defaults_fields(self, field, campo_default):
+        recipe = Recipe(
+            title='recipe title',
+            description='description',
+            slug='slug',
+            preparation_time=2,
+            preparation_time_unit='Minutos',
+            servings=3,
+            servings_unit='Pessoas',
+            preparation_steps='preparation_steps',
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='new user')
+        )
+        recipe.full_clean()
+        recipe.save()
+        self.assertEqual(getattr(recipe, field), campo_default)
 
-        for field, max_length in fields:
-            setattr(self.recipe, field, 'A' * (max_length + 1))
-
-            with self.assertRaises(ValidationError):
-                self.recipe.full_clean()
+    def test_recipe_string_representation(self):
+        self.recipe.title = 'Teste de Representação'
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(str(self.recipe), 'Teste de Representação')
